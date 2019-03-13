dd1555bf35f2414f8c8133ad1196973494822bdb
Marco Paulo Ollivier <marcopollivier@gmail.com>
2019-03-13 19:26:26 -0300

    adiciona todos os arquivos

---
+++ src/main/java/com/github/marcopollivier/talkjunit5valeapenamigrar/TalkJunit5ValeAPenaMigrarApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcopollivier.talkjunit5valeapenamigrar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkJunit5ValeAPenaMigrarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkJunit5ValeAPenaMigrarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marcopollivier/talkjunit5valeapenamigrar/TalkJunit5ValeAPenaMigrarApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.marcopollivier.talkjunit5valeapenamigrar;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TalkJunit5ValeAPenaMigrarApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
